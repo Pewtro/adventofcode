86ab9e2824d45a6bbbe7fa387a5170d45db58230
Putro <laustsf@gmail.com>
2020-12-07 12:46:44 +0100

    [Day6] Add Day 6 solution

---
+++ 2020/6/main.ts
@@ -0,0 +1,53 @@
+import { match } from 'assert';
+import fs = require('fs');
+
+const file = fs.readFileSync('input.txt').toString().split("\n\n")
+
+//Since node.js doesn't support .replaceAll we make our own variant of it
+function replaceAll(string: string, search: string, replace: string) {
+    return string.split(search).join(replace)
+}
+
+//region Part 1
+let sumOfCount = 0;
+for (let travelGroup of file) {
+    travelGroup = replaceAll(travelGroup, "\n", "")
+    let matchedLetters = []
+    for (let i = 0; i < travelGroup.length; i++) {
+        if (!matchedLetters.includes(travelGroup[i])) {
+            matchedLetters.push(travelGroup[i])
+        }
+    }
+    sumOfCount += matchedLetters.length;
+}
+console.log(sumOfCount)
+//endregion
+
+//region Part 2
+let sumOfCount2 = 0;
+for (let travelGroup of file) {
+    let peopleInGroup = travelGroup.split("\n")
+    let letterCount = {}
+    for (const idx in peopleInGroup) {
+        if(peopleInGroup[idx].length === 0) {
+            peopleInGroup.splice(parseInt(idx), 1)
+            continue;
+        }
+        for (let i = 0; i < peopleInGroup[idx].length; i++) {
+            if (letterCount[peopleInGroup[idx][i]]) {
+                letterCount[peopleInGroup[idx][i]] += 1
+            } else {
+                letterCount[peopleInGroup[idx][i]] = 1
+            }
+        }
+    }
+    for(const letter in letterCount){
+        if(letterCount[letter] === peopleInGroup.length) {
+            sumOfCount2 += 1;
+        }
+    }
+}
+console.log(sumOfCount2)
+
+
+//endregion
