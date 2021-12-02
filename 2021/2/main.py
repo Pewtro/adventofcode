3a4b7f2ee3244ee582ec73541010223d49335fd8
Putro <laustsf@gmail.com>
2021-12-02 19:34:44 +0100

    [Day2] Part 2

--- 2021/2/main.py
+++ 2021/2/main.py
@@ -9,6 +9,8 @@ f = open('tests/input.in', "r")
 
 horizontal = 0
 depth = 0
+aimDepth = 0
+aim = 0
 
 for x in f: 
   split = x.split(" ")
@@ -16,11 +18,21 @@ for x in f:
   motion = split[0]
   if(motion == Direction.up):
     depth = depth - incNum
+    aim = aim - incNum
   if(motion == Direction.down): 
     depth = depth + incNum
+    aim = aim + incNum
   if(motion == Direction.forward):
     horizontal = horizontal + incNum
+    aimDepth = aimDepth + (aim * incNum)
 
+
+print("---------- Part One -----------")
 print("Horizontal: " + str(horizontal))
 print("Depth: " + str(depth))
 print("Part 1 answer: " + str(horizontal*depth))
+
+print("---------- Part Two -----------")
+print("Aim: " + str(aim))
+print("Depth w/ Aim: " + str(aimDepth))
+print("Part 2 answer: " + str(horizontal*aimDepth))
